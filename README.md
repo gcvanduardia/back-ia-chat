fa6cd7d3b33c5d7ed486377972f0bd21c4f84659
David Suarez <gcvanguardia@gmail.com>
2024-05-12 19:47:53 -0500

    feat: requeriments

---
+++ README.md
@@ -0,0 +1,78 @@
+# START PROJECT #
+
+## STEP 1 (first time) Install virtual environment (virtualenv)
+```shell 
+pip3 install virtualenv
+```
+
+## STEP 2 (first time) Create virtual enviroment
+```shell
+python -m venv venv
+```
+
+## STEP 3 (every time) activate virtual mode
+MacOs:
+```shell
+source venv/bin/activate
+```
+Windows:
+```shell
+.\venv\Scripts\activate
+```
+
+## STEP 4 (first time) Install libraries
+```shell
+pip install -r requirements.txt
+```
+if ssl certificate problem:
+```shell
+pip install -r requirements.txt --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host=files.pythonhosted.org
+```
+
+
+## STEP 5 (every time)  Locate the input files
+Locate the input files in `data/input`. Items: 
+- **legacy**: List of legacy projects `*.xlsx`
+- **projects**: 'output file from the Coding Standards process named `*-cs-prs-detail*.csv'`
+- **sonar**: 'sonar report corresponding to the analysis month named `Reporte_Metricas_Sonar-*.xlsx'` 
+
+* Multiple files can be uploaded for each item and the process will concatenate them into a single file for each item.
+
+## STEP 6 (every time) Initialize main program
+```shell
+python run.py
+```
+
+## STEP 7 (every time) Read the output file
+The output is a file located in `data/output` named `quality-gates_*.xlsx`.
+
+
+
+# UNIT TESTING #
+
+## SET 1 (every time) Execute tests
+```shell
+python run_tests.py
+```
+
+## SET 2 (every time) Execute coverage script 
+```shell
+coverage run run_tests.py
+```
+
+## SET 3 (every time) Execute coverage report
+```shell
+coverage report -m
+```
+
+## SET 4 (every time) Generate coverage HTML report
+```shell
+coverage html
+```
+
+
+
+
+
+
+
---
+++ ia/comment_sentiment_analyzer_davivienda.py
@@ -0,0 +1,68 @@
+import re
+from transformers import pipeline
+from transformers import AutoTokenizer
+from tqdm import tqdm
+import glob
+import pandas as pd
+from datetime import datetime
+
+# Cargar la pipeline de análisis de sentimientos, que incluye el modelo y el tokenizador
+sentiment_pipeline = pipeline("text-classification", model="lxyuan/distilbert-base-multilingual-cased-sentiments-student")
+# Cargar el tokenizador
+tokenizer = AutoTokenizer.from_pretrained("lxyuan/distilbert-base-multilingual-cased-sentiments-student")
+
+def getInput():
+    excel_files = glob.glob(f'input_davivienda/*.xlsx')
+    if excel_files:
+        df = pd.read_excel(excel_files[0])
+        df_filtered = df.copy()
+        return df_filtered
+    else:
+        print(f"No se encontraron archivos de Excel en la carpeta input")
+        return pd.DataFrame()
+
+def normalize_comment(comment):
+    # Asegúrate de que comment es una cadena de texto
+    if not isinstance(comment, str):
+        comment = str(comment)
+    # Convierte a minúsculas
+    """ comment = comment.lower() """
+    # Elimina caracteres especiales (manteniendo espacios y alfanuméricos)
+    """ comment = re.sub(r'[^a-z0-9\s]', '', comment) """
+    # Reemplaza múltiples espacios con uno solo
+    comment = re.sub(r'\s+', ' ', comment).strip()
+    # Truncar a 512 tokens
+    tokens = tokenizer(comment, truncation=True, max_length=400)
+    comment = tokenizer.decode(tokens['input_ids'])
+    return comment
+
+def analyze_comments(df):
+    df = df.copy()
+    df.loc[:, 'Comment'] = df['Comment'].fillna('').astype(str)
+    df.loc[:, 'Concatenado'] = df.apply(lambda row: (". Comment: " + row['Comment'] if row['Comment'] != '' else ""), axis=1)
+
+    # Analizar el sentimiento de cada 'Concatenado' y almacenar los resultados en las nuevas columnas
+    tqdm.pandas()  # Habilita la barra de progreso para `progress_apply`
+    results = df.progress_apply(lambda row: analyze_if_valid(row['Concatenado'], row['Comment']), axis=1)
+
+    # Separar los resultados en dos columnas 'label' y 'score'
+    df['label'] = results.apply(lambda x: x['label'])
+    df['score'] = results.apply(lambda x: x['score'])
+
+    return df
+
+def analyze_if_valid(concatenado, comentario):
+    if (isinstance(comentario, str) and len(comentario) > 3):
+        return sentiment_pipeline(normalize_comment(concatenado))[0]
+    else:
+        return {'label': 'none', 'score': 0}
+
+def main():
+    comments = getInput()
+    analyzed_comments = analyze_comments(comments)
+    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    filename = f'output_pos_ia_davivienda/file_{timestamp}.xlsx'
+    analyzed_comments.to_excel(filename, index=False)
+
+if __name__ == "__main__":
+    main()
